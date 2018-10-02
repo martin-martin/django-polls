cdd5604c60a0876384e85454781070eb8c1f2099
martin-martin <breuss.martin@gmail.com>
2018-10-02 23:01:10 +0200

    create indext template and view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
-    http = ""
-    for q in Question.objects.all():
-        http += q.question_text + "<br>"
-    return HttpResponse(http)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
