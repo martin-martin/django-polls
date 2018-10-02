3cb40b1ecdee79a8ef0ef54677f4f5b24dcbac1f
martin-martin <breuss.martin@gmail.com>
2018-10-02 22:54:03 +0200

    add more views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from .models import Question
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("hei there everyone 😸!")
+    http = ""
+    for q in Question.objects.all():
+        http += q.question_text + "<br>"
+    return HttpResponse(http)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
